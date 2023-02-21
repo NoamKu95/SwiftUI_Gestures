66c2defd8414311dcebe8d03372e179cd8303c6a
Noam Kurtzer <noamkurtzer@gmail.com>
2023-02-21 10:53:18 +0200

    Added resources

---
+++ Gestures/Gestures/GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesApp.swift
+//  Gestures
+//
+//  Created by Noam Kurtzer on 21/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
